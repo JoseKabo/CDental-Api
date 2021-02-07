f7166a567bd9701c0b5e10180d8004ab25cf3fed
Jose Kabo <kabo.0699@gmail.com>
2021-02-06 20:32:11 -0600

    ver carnets

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,9 +3,11 @@ const app = express();
 
 const dentista = require('../routes/app/cd.dentista/signin');
 const dentista_stadistics = require('../routes/app/cd.dentista/home.stadistics');
+const dentista_carnets = require('./app/cd.dentista/carnets');
 
 app.use('/dentista', dentista);
 app.use('/dentista', dentista_stadistics);
+app.use('/dentista', dentista_carnets);
 
 
 module.exports = app;
