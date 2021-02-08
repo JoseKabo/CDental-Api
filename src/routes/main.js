6d165f0857bfbeacee1afaadd4cee1c9c745f2d4
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2021-02-07 21:33:43 -0600

    servicios y subs servicios

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,9 +3,13 @@ const app = express();
 
 const dentista = require('../routes/app/cd.dentista/signin');
 const dentista_stadistics = require('../routes/app/cd.dentista/home.stadistics');
+const services = require('../routes/app/cd.dentista/services');
+const subServices = require('../routes/app/cd.dentista/sub.services');
 
 app.use('/dentista', dentista);
 app.use('/dentista', dentista_stadistics);
+app.use('/dentista', services);
+app.use('/dentista', subServices);
 
 
 module.exports = app;
